c25e2cf018fdc1f76d7f07bee0f27437c5a8f225
Balloonguy <balloonguy123@gmail.com>
2013-02-09 19:05:05 -0500

    allow echoing in build scripts

--- etc/build+names.bat
+++ etc/build+names.bat
@@ -1,3 +1,3 @@
 perl build.pl Rikaichan.Android > Rikaichan.Android+jpen+jpname\bootstrap.js
 perl -pi.bak -e "s/(USING_NAMES = )(false|true)/$1true/" Rikaichan.Android+jpen+jpname\bootstrap.js && del Rikaichan.Android+jpen+jpname\bootstrap.js.bak
-perl xpi.pl Rikaichan.Android+jpen+jpname no
+perl xpi.pl Rikaichan.Android+jpen+jpname
--- etc/build.bat
+++ etc/build.bat
@@ -1,4 +1,3 @@
-@ECHO OFF
 perl build.pl Rikaichan.Android > Rikaichan.Android+jpen\bootstrap.js
 perl -pi.bak -e "s/(USING_NAMES = )(false|true)/$1false/" Rikaichan.Android+jpen\bootstrap.js && del Rikaichan.Android+jpen\bootstrap.js.bak
 perl xpi.pl Rikaichan.Android+jpen
